b393ce09bf174764f7df957f29881192313c2435
Александр <kraus.sania2015@yandex.ru>
2024-02-25 09:32:40 +0700

    Залив на гит

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.Save = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(439, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(144, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(771, 213);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(731, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Draw as lines";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DrawAsLines_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(731, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Draw as spline";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DrawAsSpline_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Draw as lines",
+            "Draw as spline"});
+            this.comboBox1.Location = new System.Drawing.Point(453, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.ListChanged += new System.ComponentModel.ListChangedEventHandler(this.MyData);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(491, 213);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(104, 23);
+            this.Save.TabIndex = 6;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(616, 213);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(104, 23);
+            this.Load.TabIndex = 7;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(589, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(262, 150);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.IsDockedInsideChartArea = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 16);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.LabelBackColor = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueMember = "X";
+            series1.YValueMembers = "Y";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(421, 329);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 361);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private Chart chart1;
+        private SeriesChartType currentChartType = SeriesChartType.Line;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public BindingSource Data { get; set; }
+
+        Title title = new Title();
+
+        public Form1()
+        {
+            Data = new BindingSource();
+
+            InitializeComponent();
+            DataGridViewTextBoxColumn fileNameColumn = new DataGridViewTextBoxColumn();
+            fileNameColumn.HeaderText = "Имя файла";
+            dataGridView1.Columns.Add(fileNameColumn);
+
+            dataGridView2.DataSource = Data;
+            Data.ListChanged += new ListChangedEventHandler(MyData);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog1.FileName.ToString());
+
+                for (int i = 0; i < dataGridView2.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView2.Columns.Count; j++)
+                    {
+                        file.Write(dataGridView2.Rows[i].Cells[j].Value.ToString() + " ");
+                    }
+                    file.WriteLine("");
+                }
+                file.Close();
+                MessageBox.Show("Файл успешно сохранен", "Успешная операция", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedValue = comboBox1.SelectedItem.ToString();
+
+                if (selectedValue == "Draw as lines")
+                {
+                    currentChartType = SeriesChartType.Line;
+                }
+                else if (selectedValue == "Draw as spline")
+                {
+                    currentChartType = SeriesChartType.Spline;
+                }
+
+                foreach (var series in chart1.Series)
+                {
+                    series.ChartType = currentChartType;
+                }
+
+                chart1.DataSource = null;
+                chart1.DataSource = Data;
+            }
+        }
+        private void MyData(object sender, ListChangedEventArgs e)
+        {
+            comboBox1_SelectedIndexChanged(sender, e);
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                dataGridView2.Rows.Clear();
+                string fileName = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+                List<string> fileContent = ReadFileContent(filePath);
+                AddFileContentToDataGridView2(fileContent);  
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string fileName in openFileDialog.FileNames)
+                    {
+                        dataGridView1.Rows.Add(Path.GetFileName(fileName));
+                        dataGridView2.Rows.Clear();
+
+                        List<string> fileContent = ReadFileContent(fileName);
+                        AddFileContentToDataGridView2(fileContent);
+
+                        Series loadedSeries = CreateChartSeries(Data, Path.GetFileName(fileName));
+                    }
+                }
+            }
+        }
+
+        private List<string> ReadFileContent(string filePath)
+        {
+            List<string> fileContent = new List<string>();
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        fileContent.Add(line);
+                    }
+                }
+            }
+            return fileContent;
+        }
+
+        private void AddFileContentToDataGridView2(List<string> fileContent)
+        {
+            foreach (string line in fileContent)
+            {
+                var values = line.Split(' '); // предполагаем, что значения разделены табуляцией
+                double xx = double.Parse(values[0]), yy = double.Parse(values[1]);
+
+                Data.Add(new Data() { X = xx, Y = yy });
+
+                BindingSource loadedTable = new BindingSource();
+                loadedTable.DataSource = Data;
+
+                dataGridView2.DataSource = Data;
+            }
+        }
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Data.Add(new Data() { X = 0, Y = 0 });
+        }
+        private void DrawAsLines_Click(object sender, EventArgs e)
+        {
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+            System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.DataSource = Data;
+        }
+        private void DrawAsSpline_Click(object sender, EventArgs e)
+        {
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+            System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.DataSource = Data;
+        }
+
+        private Series CreateChartSeries(BindingSource table, string seriesName)
+        {
+            Series series = new Series(seriesName);
+            series.ChartType = currentChartType;
+
+            foreach (Data dataPoint in table.List)
+            {
+                series.Points.AddXY(dataPoint.X, dataPoint.Y);
+            }
+
+            chart1.Series.Add(series);
+            return series;
+        }
+    }
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
